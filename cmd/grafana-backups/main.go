3e3c15880340f886996a22e37cb5ad7415f4a72c
Jillian <jkarner@letsencrypt.org>
2019-07-03 11:06:20 -0700

    Grafana dashboard backups (#5)
    Tool to backup all of the grafana dashboards for a given instance

---
+++ cmd/grafana-backups/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+	"time"
+
+	"github.com/letsencrypt/sre-tools/cmd"
+)
+
+var timeout, _ = time.ParseDuration("15s")
+
+const (
+	dashboardsByUID = "/api/dashboards/uid/"
+	allDashboards   = "/api/search?dash-db"
+)
+
+// writeDashboardFile queries the Grafana instance by a dashboard's UID for
+// the raw JSON and writes the result to a specified directory
+func writeDashboardFile(outputDirectory, uid, url, apiKey string) error {
+	body, err := fetch(url+dashboardsByUID+uid, apiKey)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(path.Join(outputDirectory, uid+".json"), body, 0644)
+}
+
+// fetch uses a fully qualified URL and api key to query a Grafana instance
+// for information at a given path. It returns the resulting body
+func fetch(url, apiKey string) ([]byte, error) {
+	client := http.Client{
+		Timeout: timeout,
+	}
+
+	request, err := http.NewRequest("GET", url, nil)
+	request.Header.Set("Authorization", "Bearer "+apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("Invalid response code %d: %s", resp.StatusCode, body)
+	}
+	return body, nil
+}
+
+// checkEnv checks that the environment variables are set and returns an error
+// if they are not. We need to use an API Key to access Grafana and the cleanest
+// way to set it is with an environment variable so it won't be visible in the
+// process flags. While we're creating one environment variable, we might as
+// well set the remaining variables instead of having a mix of environment
+// variables and flags.
+func checkEnv() error {
+	if os.Getenv("GRAFANA_URL") == "" || os.Getenv("GRAFANA_API_KEY") == "" || os.Getenv("GRAFANA_BACKUP_DIR") == "" {
+		return errors.New("Environment variables GRAFANA_URL, GRAFANA_API_KEY, and GRAFANA_BACKUP_DIR must be set")
+	}
+	return nil
+}
+
+func main() {
+	err := checkEnv()
+	cmd.FailOnError(err, "environment variables")
+	url := os.Getenv("GRAFANA_URL")
+	apiKey := os.Getenv("GRAFANA_API_KEY")
+	backupDir := os.Getenv("GRAFANA_BACKUP_DIR")
+
+	body, err := fetch(url+allDashboards, apiKey)
+	cmd.FailOnError(err, "fetching dashboards")
+
+	type dbItem struct {
+		UID string
+	}
+
+	var items []dbItem
+	err = json.Unmarshal(body, &items)
+	cmd.FailOnError(err, "Unmarshalling JSON body")
+
+	if len(items) == 0 {
+		os.Exit(1)
+	}
+
+	for _, dashboard := range items {
+		err := writeDashboardFile(backupDir, dashboard.UID, url, apiKey)
+		cmd.FailOnError(err, "Writing Dashboard fles")
+	}
+}
---
+++ cmd/grafana-backups/main_test.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestWriteDashboardFile(t *testing.T) {
+	expected := "Successful Response"
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/api/dashboards/uid/fakeUID" {
+			_, err := io.WriteString(w, expected)
+			if err != nil {
+				t.Fatal(err)
+			}
+		} else {
+			_, err := io.WriteString(w, "Bad response for providing an incorrect path")
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+	}))
+	defer ts.Close()
+
+	dir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	err = writeDashboardFile(dir, "fakeUID", ts.URL, "test")
+	if err != nil {
+		t.Errorf("writeDashboardFile failed")
+	}
+	content, err := ioutil.ReadFile(dir + "/fakeUID.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(content) != "Successful Response" {
+		t.Errorf("Expected %q. Got %q", expected, string(content))
+	}
+}
+
+func TestWriteDashboardFileError(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(500)
+	}))
+	defer ts.Close()
+
+	dir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	err = writeDashboardFile(dir, "FakeUID", ts.URL, "test")
+	if err == nil {
+		t.Error("Expected error, got none")
+	}
+	if !strings.Contains(err.Error(), "Invalid response code") {
+		t.Errorf("Expected 'Invalid response code', got %q", err)
+	}
+	_, err = os.Stat(dir + "/FakeUID")
+	if err == nil {
+		t.Error("Expected error, got none")
+	}
+	if !os.IsNotExist(err) {
+		t.Errorf("Expected `no such file or directory` got %q", err)
+	}
+}
+
+func TestCheckEnvironment(t *testing.T) {
+	os.Setenv("GRAFANA_URL", "fakeserver.com")
+	os.Setenv("GRAFANA_API_KEY", "test")
+	os.Setenv("GRAFANA_BACKUP_DIR", "/tmp/backup")
+
+	if err := checkEnv(); err != nil {
+		t.Errorf("Expected no error, go %q", err)
+	}
+}
+
+func TestNoEnvironmentSet(t *testing.T) {
+	os.Clearenv()
+	err := checkEnv()
+
+	if !strings.Contains(err.Error(), "Environment variables") {
+		t.Errorf("Expected `Environment variables` got %q", err)
+	}
+}
+
+func TestFetchGoodAuthorization(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authorization := r.Header["Authorization"][0]
+		if !strings.HasPrefix(authorization, "Bearer fake key") {
+			w.WriteHeader(400)
+		}
+	}))
+	defer ts.Close()
+
+	if _, err := fetch(ts.URL+"/grafana", "fake key"); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestFetchBadAuthorization(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authorization := r.Header["Authorization"][0]
+		if !strings.HasPrefix(authorization, "Bearer fake key") {
+			w.WriteHeader(400)
+		}
+	}))
+	defer ts.Close()
+	var err error
+
+	if _, err = fetch(ts.URL+"/grafana", "bad key"); err == nil {
+		t.Fatal("expected error, got none")
+	}
+
+	if !strings.Contains(err.Error(), "Invalid response code") {
+		t.Errorf("Expected 'invalid response code' got %q", err)
+	}
+}
